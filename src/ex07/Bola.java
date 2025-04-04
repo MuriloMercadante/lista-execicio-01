31df98e9de24b990eb8b5d9fb78327bf4830de14
MuriloMercadante <murilomercadante819@gmail.com>
2025-04-03 21:28:08 -0300

    finalizado

---
+++ src/ex07/Bola.java
@@ -0,0 +1,29 @@
+public class Bola {
+    private String cor;
+    private double raio;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setValor(double valor) {
+        this.raio = raio;
+    }
+    public double class maiorBola(Bola bola1 , Bola bola2,Bola bola3){
+        double  aux = 0;
+
+        return aux;
+    }
+
+}
+
+
+
